448d8400ccc3cc170b87e5525d3c6ee0c95c1c4f
arkie <380859982@qq.com>
2017-04-20 14:34:20 +0800

    try devtools livereload and restart customize info content

--- myboot/src/main/java/com/myboot/MybootApplication.java
+++ myboot/src/main/java/com/myboot/MybootApplication.java
@@ -7,13 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MybootApplication {
 
 	public static void main(String[] args) {
-		//System.out.println(System.getProperty("java.class.path"));
-		/*try {
-			Thread.currentThread().getContextClassLoader().loadClass("org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+	
 		SpringApplication.run(MybootApplication.class, args);
 	}
 }
