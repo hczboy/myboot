95a345d5370bc5c861e3827db94443f396f4ce6a
arkie <380859982@qq.com>
2017-04-27 00:28:28 +0800

    introduce dom4j to get text value of xml from request body

--- myboot/src/main/java/com/myboot/servlet/MyServlet.java
+++ myboot/src/main/java/com/myboot/servlet/MyServlet.java
@@ -2,6 +2,7 @@ package com.myboot.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import com.myboot.config.AmazonProperties;
 
@@ -29,16 +37,50 @@ public class MyServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("---------------doGet---------");
-		doPost(req, resp);
+		//doPost(req, resp);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("+++++++++++++++++doPost+++++++++++");
+	
+		SAXReader reader = new SAXReader();
+		reader.setErrorHandler(new ErrorHandler(){
+
+			@Override
+			public void warning(SAXParseException exception) throws SAXException {
+				exception.printStackTrace();
+				
+			}
+
+			@Override
+			public void error(SAXParseException exception) throws SAXException {
+				exception.printStackTrace();
+				
+			}
+
+			@Override
+			public void fatalError(SAXParseException exception) throws SAXException {
+				exception.printStackTrace();
+				
+				
+			}
+			
+		});
+		String msg = null;
+		try {
+			Document doc = reader.read(req.getInputStream());
+			msg = doc.selectSingleNode("//ProvisionRequestMessage/serialNumber").getText();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+     /*   String str = IOUtils.toString(req.getReader());
+			System.out.println(str);*/
 		resp.setContentType("text/html");
 		PrintWriter pw = resp.getWriter();
-		pw.println("servlet output:");
-		pw.print(amazonProp.getAssociateId());
+		
+		pw.println(msg);
 	}
 }
