0a5d1780da17eb1264a0757b2515effb411cfed6
arkie <380859982@qq.com>
2017-05-09 18:20:15 +0800

    test if ApplicationContext is injected without any other action

--- myboot/src/main/java/com/myboot/controller/AdviceController.java
+++ myboot/src/main/java/com/myboot/controller/AdviceController.java
@@ -2,11 +2,13 @@ package com.myboot.controller;
 
 import org.myspring.starter.hello.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.myboot.config.AmazonProperties;
 import com.myboot.config.MyEnv;
 import com.myboot.vo.DemoObj;
 
@@ -21,6 +23,9 @@ public class AdviceController {
 
 	@Autowired
 	private HelloService helloService;
+
+	@Autowired
+	private ApplicationContext ctx;
 	
 	@RequestMapping(value="/hello")
 	public @ResponseBody String hello(){
@@ -29,6 +34,8 @@ public class AdviceController {
 	
 	@RequestMapping("/amazonProp")
 	public @ResponseBody String getAmazonProp(){
-		return MyEnv.getAmazonProperties().getAssociateId();
+		//return MyEnv.getAmazonProperties().getAssociateId();
+		AmazonProperties amazonProp = (AmazonProperties)ctx.getBean("amazonProperties");
+		return amazonProp.getAssociateId();
 	}
 }
